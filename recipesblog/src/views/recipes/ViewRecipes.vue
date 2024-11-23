30eb692c304de27b5feba839dc9e7dd7cdff8747
Dimitar Iliev <dimitar.iliev@accedia.com>
2024-11-23 20:08:46 +0200

    Fix ViewRecipes view

--- recipesblog/src/views/recipes/ViewRecipes.vue
+++ recipesblog/src/views/recipes/ViewRecipes.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="navbar-item is-size-4 is-family-monospace">Recipes</div>
-  <div class="columns is-8">
-    <div v-for="recipe in recipes" class="column">
-      <div class="card">
-        <Recipe :recipe="recipe" :key="recipe.name" />
+  <div class="fixed-grid has-3-cols">
+    <div class="grid is-gap-5">
+      <div v-for="recipe in recipes" :key="recipe.name" class="cell">
+        <div class="card">
+          <Recipe :recipe="recipe" />
+        </div>
       </div>
     </div>
   </div>
@@ -14,10 +16,11 @@ import Recipe from "@/components/recipes/Recipe.vue";
 const recipesConst = [
   {
     id: 1,
-    name: "Test Name",
+    name: "Musaka",
     servings: 2,
     hours: 2,
     minutes: 20,
+    likes: 40,
     ingredients: ["butter", "suggar"],
     directions: ["first direction", "second direction"],
     notes: "Test note",
@@ -25,10 +28,11 @@ const recipesConst = [
   },
   {
     id: 2,
-    name: "Test Name1",
+    name: "Kavarma",
     servings: 3,
     hours: 4,
     minutes: 50,
+    likes: 20,
     ingredients: ["butter", "suggar"],
     directions: ["first direction", "second direction"],
     notes: "Test note2",
@@ -36,15 +40,40 @@ const recipesConst = [
   },
   {
     id: 3,
-    name: "Test Name2",
+    name: "Blister",
     servings: 4,
     hours: 4,
     minutes: 50,
     ingredients: ["butter", "suggar"],
     directions: ["first direction", "second direction"],
     notes: "Test note2",
+    likes: 5,
     image: "https://www.supichka.com/files/images/1242/musaka_2.jpg",
-  }
+  },
+  {
+    id: 4,
+    name: "Blister2",
+    servings: 5,
+    hours: 4,
+    minutes: 50,
+    ingredients: ["butter", "suggar", "oat"],
+    directions: ["first direction", "second direction"],
+    notes: "Test note2",
+    likes: 100,
+    image: "https://www.supichka.com/files/images/1242/musaka_2.jpg",
+  },
+  {
+    id: 4,
+    name: "Blister2",
+    servings: 5,
+    hours: 4,
+    minutes: 50,
+    ingredients: ["butter", "suggar", "oat"],
+    directions: ["first direction", "second direction"],
+    notes: "Test note2",
+    likes: 100,
+    image: "https://www.supichka.com/files/images/1242/musaka_2.jpg",
+  },
 ];
 export default {
   components: {
@@ -52,7 +81,7 @@ export default {
   },
   data() {
     return {
-      recipes: recipesConst,
+      recipes: recipesConst
     };
   },
 };
