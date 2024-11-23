6b1e11651fe050d6b0adc7dc1c5257fd976deff1
Dimitar Iliev <dimitar.iliev@accedia.com>
2024-11-23 20:08:28 +0200

    Modify the Recipe component

--- recipesblog/src/components/recipes/Recipe.vue
+++ recipesblog/src/components/recipes/Recipe.vue
@@ -6,7 +6,21 @@
   </div>
   <div class="card-content">
     <div class="content">
-      {{ recipe.notes }}
+      <h3>{{ recipe.name }}</h3>
+      <div class="columns">
+        <div class="column">
+            <span class="icon">
+                <i class="fa-solid fa-heart"></i>
+            </span>
+            {{ recipe.likes }}
+        </div>
+        <div class="column">
+            <span class="icon">
+              <i class="fas fa-clock"></i>
+            </span>
+            {{ recipe.hours }} hours {{ recipe.minutes }} minutes
+        </div>
+      </div>
     </div>
   </div>
   <footer class="card-footer">
@@ -23,6 +37,6 @@ export default {
       type: Object,
       required: true,
     },
-  },
+  }
 };
 </script>
