452722ee11e0efccf2518d63febde11b2bc10c90
Dimitar Iliev <dimitar.iliev@accedia.com>
2024-11-26 13:35:22 +0200

    Implement search on category tabs

---
+++ recipesblog/src/components/categories/CategoryTabs.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="tabs is-boxed">
+    <ul>
+      <li
+        v-for="category in categories"
+        :key="category.name"
+        :class="{ 'is-active': category.isActive }"
+        @click="selectCategoryName(category)"
+      >
+        <a>
+          <span class="icon">
+            <img :src="category.image" />
+          </span>
+          <span>{{ category.name }}</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+const categories = [
+  {
+    name: "All",
+    image: "/src/assets/categories/maincourse.png",
+    isActive: false,
+  },
+  {
+    name: "Salads",
+    image: "/src/assets/categories/maincourse.png",
+    isActive: false,
+  },
+  {
+    name: "Main course",
+    image: "/src/assets/categories/maincourse.png",
+    isActive: false,
+  },
+  {
+    name: "Bakery",
+    image: "/src/assets/categories/bakery.png",
+    isActive: false,
+  },
+  {
+    name: "Soups",
+    image: "/src/assets/categories/maincourse.png",
+    isActive: false,
+  },
+  {
+    name: "Desserts",
+    image: "/src/assets/categories/maincourse.png",
+    isActive: false,
+  },
+  {
+    name: "Drinks",
+    image: "/src/assets/categories/drinks.png",
+    isActive: false,
+  },
+];
+export default {
+  emits: ["selectCategoryName"],
+  data() {
+    return {
+      categories,
+    };
+  },
+  methods: {
+    selectCategoryName(category) {
+      category.isActive = !category.isActive;
+      this.$emit("selectCategoryName", category.name);
+    },
+  },
+};
+</script>
--- recipesblog/src/views/recipes/ViewRecipes.vue
+++ recipesblog/src/views/recipes/ViewRecipes.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="navbar-item is-size-4 is-family-monospace">Recipes</div>
+  <CategoryTabs v-on:select-category-name="onSelectCategoryNameHandler" />
   <SearchBar v-model="recipeName" />
   <div class="fixed-grid has-3-cols">
     <div class="grid is-gap-5">
-      <div v-for="recipe in filteredRecipesByName" :key="recipe.name" class="cell">
+      <div
+        v-for="recipe in filteredRecipesByName"
+        :key="recipe.name"
+        class="cell"
+      >
         <div class="card">
           <Recipe :recipe="recipe" />
         </div>
@@ -13,6 +18,7 @@
 </template>
 
 <script>
+import CategoryTabs from "@/components/categories/CategoryTabs.vue";
 import Recipe from "@/components/recipes/Recipe.vue";
 import SearchBar from "@/components/SearchBar.vue";
 const recipesConst = [
@@ -22,6 +28,7 @@ const recipesConst = [
     servings: 2,
     hours: 2,
     minutes: 20,
+    categoryName: "Main course",
     likes: 40,
     ingredients: ["butter", "suggar"],
     directions: ["first direction", "second direction"],
@@ -34,6 +41,7 @@ const recipesConst = [
     servings: 3,
     hours: 4,
     minutes: 50,
+    categoryName: "Main course",
     likes: 20,
     ingredients: ["butter", "suggar"],
     directions: ["first direction", "second direction"],
@@ -46,6 +54,7 @@ const recipesConst = [
     servings: 4,
     hours: 4,
     minutes: 50,
+    categoryName: "Desserts",
     ingredients: ["butter", "suggar"],
     directions: ["first direction", "second direction"],
     notes: "Test note2",
@@ -58,6 +67,7 @@ const recipesConst = [
     servings: 5,
     hours: 4,
     minutes: 50,
+    categoryName: "Desserts",
     ingredients: ["butter", "suggar", "oat"],
     directions: ["first direction", "second direction"],
     notes: "Test note2",
@@ -66,10 +76,11 @@ const recipesConst = [
   },
   {
     id: 4,
-    name: "Blister2",
+    name: "Blister3",
     servings: 5,
     hours: 4,
     minutes: 50,
+    categoryName: "Desserts",
     ingredients: ["butter", "suggar", "oat"],
     directions: ["first direction", "second direction"],
     notes: "Test note2",
@@ -81,19 +92,37 @@ export default {
   components: {
     Recipe,
     SearchBar,
+    CategoryTabs,
   },
   data() {
     return {
       recipes: recipesConst,
       recipeName: "",
+      selectedCategoryName: "",
     };
+  },
+  methods: {
+    onSelectCategoryNameHandler(selectedCategoryName) {
+      console.log("Handler", selectedCategoryName);
+      this.selectedCategoryName = selectedCategoryName;
+    },
   },
   computed: {
     filteredRecipesByName() {
-      return this.recipes.filter((r) => {
-        return (
-          r.name.toLowerCase().indexOf(this.recipeName.toLowerCase()) != -1
-        );
+      return this.recipes.filter((recipe) => {
+        const matchesName = recipe.name
+          .toLowerCase()
+          .includes(this.recipeName.toLowerCase());
+
+        if (this.selectedCategoryName === "All") {
+          this.selectedCategoryName = "";
+        }
+
+        const matchesCategory =
+          this.selectedCategoryName === "" ||
+          recipe.categoryName === this.selectedCategoryName;
+
+        return matchesName && matchesCategory;
       });
     },
   },
