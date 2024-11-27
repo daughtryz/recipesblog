da59adf2fc01320af8d15222e642b3087909c58a
Dimitar Iliev <dimitar.iliev@accedia.com>
2024-11-27 14:39:00 +0200

    Add RecipeInputFiled

---
+++ recipesblog/src/components/recipes/RecipeInputField.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="field is-horizontal mt-5">
+    <div class="field-label is-normal">
+      <label :for="labelNormalized" class="label">{{ labelName }}</label>
+    </div>
+    <div class="field-body">
+      <div class="field">
+        <input
+          class="input"
+          :type="type"
+          @input="$emit('update:modelValue', $event.target.value)"
+          :value="modelValue"
+          :placeholder="placeholderName"
+        />
+        <div class="input-errors" v-for="error of errors" :key="error.$uid">
+          <div class="error-msg">{{ error.$message }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    "modelValue",
+    "labelName",
+    "placeholderName",
+    "labelNormalized",
+    "type",
+    "errors",
+  ],
+  emits: ["update:modelValue"],
+};
+</script>
+
+<style scoped>
+.input-errors {
+  color: red;
+}
+</style>
