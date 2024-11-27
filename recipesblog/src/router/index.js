f9b5e4470cbb77c518d86b51d535ac25912ed885
Dimitar Iliev <dimitar.iliev@accedia.com>
2024-11-27 14:39:25 +0200

    Configure the route for add recipe view

--- recipesblog/src/router/index.js
+++ recipesblog/src/router/index.js
@@ -1,27 +1,33 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [,
+  routes: [
+    ,
     {
-      path: '/',
-      name: 'recipes',
-      component: () => import('../views/recipes/ViewRecipes.vue')
+      path: "/",
+      name: "recipes",
+      component: () => import("../views/recipes/ViewRecipes.vue"),
     },
     {
-      path: '/recipes/details/:id',
-      name: 'recipeDetails',
-      component: () => import('../views/recipes/ViewRecipeDetails.vue')
+      path: "/recipes/details/:id",
+      name: "recipeDetails",
+      component: () => import("../views/recipes/ViewRecipeDetails.vue"),
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/recipes/add",
+      name: "addRecipe",
+      component: () => import("../views/recipes/ViewAddRecipe.vue"),
+    },
+    {
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: () => import("../views/AboutView.vue"),
     },
   ],
-})
+});
 
-export default router
+export default router;
