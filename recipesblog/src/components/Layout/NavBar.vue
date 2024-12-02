2df542a812b9550e436400d6de17785ad5ca728d
Dimitar Iliev <dimitar.iliev@accedia.com>
2024-12-02 16:06:40 +0200

    Add some styles to the navbar

--- recipesblog/src/components/Layout/NavBar.vue
+++ recipesblog/src/components/Layout/NavBar.vue
@@ -1,38 +1,11 @@
 <template>
   <nav class="navbar is-success" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      <a
-        role="button"
-        class="navbar-burger"
-        aria-label="menu"
-        aria-expanded="false"
-        data-target="navbarBasicExample"
-      >
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
-
     <div id="navbarBasicExample" class="navbar-menu">
       <div class="navbar-start">
-        <a class="navbar-item"> Recipes </a>
-
-        <a class="navbar-item"> Documentation </a>
-
-        <div class="navbar-item has-dropdown is-hoverable">
-          <a class="navbar-link"> More </a>
-
-          <div class="navbar-dropdown">
-            <a class="navbar-item"> About </a>
-            <a class="navbar-item is-selected"> Jobs </a>
-            <a class="navbar-item"> Contact </a>
-            <hr class="navbar-divider" />
-            <a class="navbar-item"> Report an issue </a>
-          </div>
-        </div>
+        <a @click.prevent="routeToRecipePage" class="navbar-item"><b>Recipes</b></a>
+        <a class="navbar-item"><b>Statistics</b></a>
       </div>
+      
 
       <div class="navbar-end">
         <div class="navbar-item">
@@ -90,7 +63,10 @@ export default {
     },
     logout() {
       this.userStore.logout();
-    }
+    },
+    routeToRecipePage() {
+      this.$router.push("/");
+    },
   },
 };
 </script>
