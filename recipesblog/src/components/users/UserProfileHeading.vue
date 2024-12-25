4c769739a5867f676874691464a8f763161f9046
Dimitar Iliev <dimitar.iliev@accedia.com>
2024-12-25 16:55:07 +0200

    Setup firebase storage

--- recipesblog/src/components/users/UserProfileHeading.vue
+++ recipesblog/src/components/users/UserProfileHeading.vue
@@ -3,17 +3,27 @@
         <div class='columns is-mobile is-multiline'>
             <div class='column is-2'>
                 <span class='header-icon user-profile-image'>
-                    <img alt='' src='http://placehold.it/300x225'>
+                    <img v-if="!userStore.user.photoURL" alt='' src='http://placehold.it/300x225'>
+                    <img v-else alt='' :src='userStore.user.photoURL'>
                 </span>
             </div>
             <div class='column is-4-tablet is-10-mobile name'>
                 <p>
                     <span class='title is-bold'>{{ userStore.user.email }}</span>
                     <br>
-                    <button class='button is-primary is-outlined' href='#' id='edit-preferences' style='margin: 5px 0'>
-                        Edit
-                    </button>
-                    <br>
+                <div class="file is-primary mt-2">
+                    <label class="file-label">
+                        <input @change="onFileChange" class="file-input" type="file" name="resume" />
+                        <span class="file-cta">
+                            <span class="file-icon">
+                                <i class="fas fa-upload"></i>
+                            </span>
+                            <span v-if="!selectedFile" class="file-label">Upload picture…</span>
+                            <button v-else @click="uploadToFirebase">Update Photo</button>
+                        </span>
+                    </label>
+                </div>
+                <br>
                 </p>
             </div>
             <div class='column is-2-tablet is-4-mobile has-text-centered'>
@@ -35,6 +45,9 @@
 <script>
 import { useUserStore } from '@/stores/storeAuth';
 import { useRecipeStore } from '@/stores/storeRecipe';
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { storage } from "@/js/firebase";
+import { getAuth, updateProfile } from 'firebase/auth';
 
 export default {
     setup() {
@@ -50,8 +63,52 @@ export default {
         return {
             myRecipesCount: 0,
             myLikedRecipes: 0,
-            myComments: 0
+            myComments: 0,
+            selectedFile: null,
+            previewUrl: null,
+            downloadUrl: null,
         }
+    },
+    methods: {
+        onFileChange(event) {
+            const file = event.target.files[0];
+            console.log('on change')
+            console.log(file)
+            if (file) {
+                this.selectedFile = file;
+                this.previewUrl = URL.createObjectURL(file);
+            }
+        },
+        async uploadToFirebase() {
+            if (!this.selectedFile)
+                return;
+
+            try {
+                const storageRef = ref(storage, `profilePictures/${this.selectedFile.name}`);
+                console.log('After storage ref')
+                console.log(storageRef)
+
+                await uploadBytes(storageRef, this.selectedFile);
+                console.log('After upload')
+                // Get the download URL of the uploaded image
+                const downloadUrl = await getDownloadURL(storageRef);
+
+                // Update the user's photoURL
+                // const auth = getAuth();
+                // const user = auth.currentUser;
+
+                if (this.userStore.user) {
+                    await updateProfile(this.userStore.user, {
+                        photoURL: downloadUrl,
+                    });
+
+                    console.log('Updated photoURL:', downloadUrl);
+                }
+            } catch (error) {
+                console.error('Error updating photoURL:', error);
+                this.successMessage = 'Error updating profile photo.';
+            }
+        },
     },
     mounted() {
         this.myRecipesCount = this.recipeStore.getRecipesByUserId(this.userStore.user.id).length;
