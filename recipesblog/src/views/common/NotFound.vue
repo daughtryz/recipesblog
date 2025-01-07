e9b281b48e8229c57a6bf74f0a6834005808deb5
Dimitar Iliev <dimitar.iliev@accedia.com>
2025-01-07 22:57:33 +0200

    Add not found page

---
+++ recipesblog/src/views/common/NotFound.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="not-found">
+        <h1>404 - Page Not Found</h1>
+        <p>Sorry, the page you are looking for does not exist.</p>
+        <router-link to="/">Go to Home</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NotFound",
+};
+</script>
+
+<style scoped>
+.not-found {
+    text-align: center;
+    margin-top: 50px;
+}
+
+.not-found h1 {
+    font-size: 3rem;
+    color: #ff6f61;
+}
+
+.not-found p {
+    font-size: 1.2rem;
+    color: #555;
+}
+
+.not-found a {
+    color: #007bff;
+    text-decoration: none;
+}
+</style>
